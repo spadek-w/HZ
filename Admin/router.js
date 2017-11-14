7c692b5c71e78b1766a3808cabc36c169772094f
Hanrea <hanrea@qq.com>
2017-11-14 14:42:14 +0800

    根据iview-admin项目裁剪出的空项目用作后台管理模板

---
+++ Admin/router.js
@@ -0,0 +1,81 @@
+import Main from './view/Main.vue';
+
+// 不作为Main组件的子页面展示的页面单独写，如下
+export const loginRouter = {
+    path: '/login',
+    name: 'login',
+    meta: {
+        title: 'Login - 登录'
+    },
+    component: resolve => { require(['./view/login.vue'], resolve); }
+};
+
+export const locking = {
+    path: '/locking',
+    name: 'locking',
+    component: resolve => { require(['./view/layout/locking-page.vue'], resolve); }
+};
+
+export const page404 = {
+    path: '/*',
+    name: 'error_404',
+    meta: {
+        title: '404-页面不存在'
+    },
+    component: resolve => { require(['./view/common/404.vue'], resolve); }
+};
+
+export const page401 = {
+    path: '/401',
+    meta: {
+        title: '401-权限不足'
+    },
+    name: 'error_401',
+    component: resolve => { require(['./view/common/401.vue'], resolve); }
+};
+
+export const page500 = {
+    path: '/500',
+    meta: {
+        title: '500-服务端错误'
+    },
+    name: 'error_500',
+    component: resolve => { require(['./view/common/500.vue'], resolve); }
+};
+
+// 作为Main组件的子页面展示但是不在左侧菜单显示的路由写在otherRouter里
+export const otherRouter = {
+    path: '/',
+    name: 'otherRouter',
+    component: Main,
+    children: [
+        { path: 'home', title: {i18n: 'home'}, name: 'home_index', component: resolve => { require(['./view/home/home.vue'], resolve); } },
+        { path: 'message', title: '消息中心', name: 'message_index', component: resolve => { require(['./view/common/message.vue'], resolve); } }
+    ]
+};
+
+// 作为Main组件的子页面展示并且在左侧菜单显示的路由写在appRouter里
+export const appRouter = [
+    {
+        path: '/access-test',
+        icon: 'lock-combination',
+        title: '权限测试页',
+        name: 'accesstest',
+        access: 0,
+        component: Main,
+        children: [
+            { path: 'index', title: '权限测试页', name: 'accesstest_index' }
+        ]
+    }
+];
+
+// 所有上面定义的路由都要写在下面的routers里
+export const routers = [
+    loginRouter,
+    otherRouter,
+    locking,
+    ...appRouter,
+    page500,
+    page401,
+    page404
+];
---
+++ ReadMe.md
@@ -0,0 +1,23 @@
+### 
+
+
+说明 多项目（每个相互用采用文件夹相互独立），多页面。后台独立为一个页面，部分组件复用（后台似乎和其他项目共性较小）
+
+
+### 目录结构
+
+    - Admin 管理后台页面
+    - Common 通用
+    - Component 组件
+    - Mock 模拟数据
+    - WWW 官网页面
+
+
+### Mock 数据
+
+数据库存储，随机返回结果
+
+
+### Admin 管理后台 
+
+基于ivew-admin实现，具体参考[](https://github.com/iview/iview-admin)
